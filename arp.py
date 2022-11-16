af91062b96f1ba206252b843a008b6fa2801988e
Shay P.C <3180102686@zju.edu.cn>
2022-11-15 21:56:54 -0700

    [format]

--- arp.py
+++ arp.py
@@ -5,11 +5,11 @@ import numpy as np
 
 # FIXME: drifting of the R matrix
 
-ti.init(arch = ti.cuda, default_fp = ti.f32)
+ti.init(arch=ti.cuda, default_fp=ti.f32)
 
 delta = 0.08
 per_trace = 10
-trajectory = ti.Vector.field(3, float, shape = (80))
+trajectory = ti.Vector.field(3, float, shape=(80))
 
 # @ti.func
 # def dot_block_diag(A, x):
@@ -23,26 +23,27 @@ trajectory = ti.Vector.field(3, float, shape = (80))
 #         A2[i, j] = A[i + 3, j + 3]
 #     for i in ti.static(range(3)):
 #         x1[i] = x[i]
-#         x2[i] = x[i + 3] 
-    
+#         x2[i] = x[i + 3]
+
 #     r1 = A1 @ x1
 #     r2 = A2 @ x2
 
 #     for i in ti.static(range(3)):
 #         ret[i] = r1[i]
 #         ret[i + 3] = r2[i]
-        
+
 #     return ret
-    
+
+
 @ti.func
 def skew(r):
     ret = ti.Matrix.zero(float, 3, 3)
-    ret[0,1] = -r[2]
-    ret[1,0] = +r[2]
-    ret[0,2] = +r[1]
-    ret[2,0] = -r[1]
-    ret[1,2] = -r[0]
-    ret[2,1] = +r[0]
+    ret[0, 1] = -r[2]
+    ret[1, 0] = +r[2]
+    ret[0, 2] = +r[1]
+    ret[2, 0] = -r[1]
+    ret[1, 2] = -r[0]
+    ret[2, 1] = +r[0]
     return ret
 
 # @ti.func
@@ -53,6 +54,7 @@ def skew(r):
 #         ret[i + 3, j + 3] = B[i, j]
 #     return ret
 
+
 @ti.func
 def unskew(R):
     ret = ti.Vector.zero(float, 3)
@@ -61,10 +63,11 @@ def unskew(R):
     ret[2] = R[1, 0]
     return ret
 
+
 @ti.func
 def f(p, q):
     return ti.Vector.zero(float, 3)
-    
+
 
 @ti.func
 def tau(p, q):
@@ -80,8 +83,8 @@ def tau(p, q):
 #         A1[i, j] = A[i, j]
 #         A2[i, j] = A[i + 3, j + 3]
 
-#     b1 = ti.Vector.zero(float, 3) 
-#     b2 = ti.Vector.zero(float, 3) 
+#     b1 = ti.Vector.zero(float, 3)
+#     b2 = ti.Vector.zero(float, 3)
 #     for i in ti.static(range(3)):
 #         b1[i] = b[i]
 #         b2[i] = b[i + 3]
@@ -93,6 +96,7 @@ def tau(p, q):
 #         ret[i + 3] = x2[i]
 
 #     return ret
+
 
 @ti.func
 def rotation(a, b, c):
@@ -114,6 +118,7 @@ def rotation(a, b, c):
     ])
     return R
 
+
 @ti.func
 def Jw(a, b, c):
     '''
@@ -126,7 +131,6 @@ def Jw(a, b, c):
     c1 = cos(a)
     c2 = cos(b)
     c3 = cos(c)
-
 
     R = ti.Matrix([
         [c1 * c2, c1 * s2 * s3 - c3 * s1, s1 * s3 + c1 * c3 * s2],
@@ -153,6 +157,7 @@ def Jw(a, b, c):
     jc = unskew(pR_pc @ R.transpose())
     return ti.Matrix.cols([ja, jb, jc])
 
+
 @ti.func
 def J_dot(a, b, c, d1, d2, d3):
     s1 = sin(a)
@@ -162,7 +167,6 @@ def J_dot(a, b, c, d1, d2, d3):
     c1 = cos(a)
     c2 = cos(b)
     c3 = cos(c)
-
 
     R = ti.Matrix([
         [c1 * c2, c1 * s2 * s3 - c3 * s1, s1 * s3 + c1 * c3 * s2],
@@ -198,7 +202,7 @@ def J_dot(a, b, c, d1, d2, d3):
         [0, 0, 0]
     ])
     pR2_pbb = ti.Matrix([
-        [c1 * -c2, c1 * -s2 * s3,c1 * c3 * -s2],
+        [c1 * -c2, c1 * -s2 * s3, c1 * c3 * -s2],
         [-c2 * s1, s1 * -s2 * s3, c3 * s1 * -s2],
         [s2, -c2 * s3, -c2 * c3]
     ])
@@ -235,22 +239,23 @@ def J_dot(a, b, c, d1, d2, d3):
 #     jw = Jw(a, b, c)
 #     return block_diag(ti.Matrix.identity(float, 3), jw)
 
+
 @ti.data_oriented
 class Cube:
-    def __init__(self, scale = [1.0, 1.0, 1.0], omega = [0.,0.,0.], pos = [0.,0.,0.], parent = None, Newton_Euler = False):
+    def __init__(self, scale=[1.0, 1.0, 1.0], omega=[0., 0., 0.], pos=[0., 0., 0.], parent=None, Newton_Euler=False):
         # generalized coordinates
-        self.p = ti.Vector.field(3, float, shape = ())
-        self.v = ti.Vector.field(3, float, shape = ())
-        self.R = ti.Matrix.field(3,3, float, shape=())
-        self.q = ti.Vector.field(6, float, shape = ())
-        self.q_dot = ti.Vector.field(6, float, shape = ())
+        self.p = ti.Vector.field(3, float, shape=())
+        self.v = ti.Vector.field(3, float, shape=())
+        self.R = ti.Matrix.field(3, 3, float, shape=())
+        self.q = ti.Vector.field(6, float, shape=())
+        self.q_dot = ti.Vector.field(6, float, shape=())
         # self.J_dot = ti.Matrix.field(6, 6, float, shape = ())
-        self.omega = ti.Vector.field(3, float, shape = ())
-        self.euler = ti.field(float, shape = (3))
-        self.euler_dot = ti.field(float, shape = (3))
+        self.omega = ti.Vector.field(3, float, shape=())
+        # self.euler = ti.field(float, shape = (3))
+        # self.euler_dot = ti.field(float, shape = (3))
         # self.Jw = ti.Matrix.field(3,3,float, shape=())
-        self.Mc = ti.field(float, shape=(6, 6))
-        
+        # self.Mc = ti.field(float, shape=(6, 6))
+
         self.p[None] = ti.Vector(pos)
         self.omega[None] = ti.Vector(omega)
         # constants
@@ -261,48 +266,45 @@ class Cube:
         self.parent = parent
         self.children = []
 
-        self.v_transformed = ti.Vector.field(3, float, shape = (8))
-        self.vertices = ti.Vector.field(3, float, shape = (8))
-        self.indices = ti.field(ti.i32, shape = (3 * 12))
-        self.faces = ti.Vector.field(4, ti.i32, shape = (6))
+        self.v_transformed = ti.Vector.field(3, float, shape=(8))
+        self.vertices = ti.Vector.field(3, float, shape=(8))
+        self.indices = ti.field(ti.i32, shape=(3 * 12))
+        self.faces = ti.Vector.field(4, ti.i32, shape=(6))
 
-        
         self.initialize()
         # self.set_Ic()
         # self.set_M()
         self.gen_v()
         self.gen_id()
         self.substep = self.midpoint if Newton_Euler else self.lagrange_midpoint
-        
+
     @ti.kernel
     def initialize(self):
-        self.v[None] = ti.Vector.zero(float , 3)
+        self.v[None] = ti.Vector.zero(float, 3)
         self.R[None] = ti.Matrix.identity(float, 3)
-        self.set_Mc()
+        # self.set_Mc()
         self.q_dot[None][3] = self.omega[None][0]
         self.q_dot[None][4] = self.omega[None][1]
         self.q_dot[None][5] = self.omega[None][2]
 
-    @ti.func
-    def set_Mc(self):
-        for i in ti.static(range(3)):
-            self.Mc[i, i] = self.m
-            self.Mc[i + 3, i + 3] = self.Ic
-            
+    # @ti.func
+    # def set_Mc(self):
+    #     for i in ti.static(range(3)):
+    #         self.Mc[i, i] = self.m
+    #         self.Mc[i + 3, i + 3] = self.Ic
 
-            
     @ti.kernel
     def midpoint(self):
         '''
         Newton-Euler Formulation
         midpoint time integral
         order of 2
-        
+
         '''
         dt = 1e-3
         dv = f(self.p[None], self.R[None]) * dt / 2 / self.m
-        dp = self.v[None] * dt 
-        domega = tau(self.p[None], self.R[None]) * dt /2 / self.Ic
+        dp = self.v[None] * dt
+        domega = tau(self.p[None], self.R[None]) * dt / 2 / self.Ic
         dq = skew(self.omega[None]) @ self.R[None] * dt / 2
 
         v_mid = dv + self.v[None]
@@ -325,27 +327,26 @@ class Cube:
         treat rotation and translation seperately in two 3*3 jacobian matrix
         '''
         _Jw = Jw(q[3], q[4], q[5])
-        
+
         # M = block_diag(ti.Matrix.identity(float, 3) * self.m, _Jw.transpose() @ _Jw * self.Ic)
         # M = J.transpose() @ self.Mc[None] @ J
         tiled_omega_BR = skew(_Jw @ ti.Vector([q_dot[3], q_dot[4], q_dot[5]]))
         # tiled_omega = block_diag(ti.Matrix.zero(float, 3,3), tiled_omega_BR)
         # C = dot_block_diag(block_diag(ti.Matrix.zero(float, 3, 3), _Jw.transpose() * self.Ic @ J_dot(q[3], q[4], q[5], q_dot[3], q_dot[4], q_dot[5]) + _Jw.transpose() @ tiled_omega_BR * self.Ic @ _Jw), q_dot)
-        
 
         # sovle M q.. + C = Q
 
         # q += q_dot
         # q_dot += solve_block_diag(M, -C)
-        
+
         q_dot_omega = ti.Vector([q_dot[3], q_dot[4], q_dot[5]])
         # q_dot_v = ti.Vector([q_dot[0], q_dot[1], q_dot[2]])
-        C_omega = (_Jw.transpose() * self.Ic @ J_dot(q[3], q[4], q[5], q_dot[3], q_dot[4], q_dot[5]) + _Jw.transpose() @ tiled_omega_BR * self.Ic @ _Jw) @ q_dot_omega
+        C_omega = (_Jw.transpose() * self.Ic @ J_dot(q[3], q[4], q[5], q_dot[3], q_dot[4],
+                   q_dot[5]) + _Jw.transpose() @ tiled_omega_BR * self.Ic @ _Jw) @ q_dot_omega
         M_omega = _Jw.transpose() @ _Jw * self.Ic
-        
+
         __q = M_omega.inverse() @ -C_omega
-        return q_dot, ti.Vector([0,0,0, __q[0], __q[1], __q[2]])
-        
+        return q_dot, ti.Vector([0, 0, 0, __q[0], __q[1], __q[2]])
 
     @ti.kernel
     def lagrange_midpoint(self):
@@ -355,39 +356,35 @@ class Cube:
         3 COM velocities + 3 euler angles (rotation X1 Z2 X3)
         '''
         dt = 1e-4
-        dq, dq_dot = self.lagrange_explicit_euler(self.q_dot[None], self.q[None])
-        
+        dq, dq_dot = self.lagrange_explicit_euler(
+            self.q_dot[None], self.q[None])
+
         q_mid = self.q[None] + dq * dt / 2
         q_dot_mid = self.q_dot[None] + dq_dot * dt / 2
 
         dq, dq_dot = self.lagrange_explicit_euler(q_dot_mid, q_mid)
-        self.q[None] += dq *dt
+        self.q[None] += dq * dt
         self.q_dot[None] += dq_dot * dt
         r = rotation(self.q[None][3], self.q[None][4], self.q[None][5])
         p = ti.Vector([self.q[None][0], self.q[None][1], self.q[None][2]])
 
         for i in ti.static(range(8)):
             self.v_transformed[i] = r @ self.vertices[i] + p
-        
-    def link(self, cube):
-        # self.parent.append(cube)
-        cube.children.append(self)
-        
+
     @ti.kernel
-    def gen_v(self ):
-        for i,j,k in ti.ndrange(2,2,2):
+    def gen_v(self):
+        for i, j, k in ti.ndrange(2, 2, 2):
             I = i * 4 + j * 2 + k
             self.vertices[I] = ti.Vector([i - 0.5, j - 0.5, k - 0.5])
-    
-        
+
     @ti.kernel
     def gen_id(self):
-        self.faces[0] = ti.Vector([0,1,3,2])
-        self.faces[1] = ti.Vector([4,5,1,0])
-        self.faces[2] = ti.Vector([2,3,7,6])
-        self.faces[3] = ti.Vector([4,0,2,6])
-        self.faces[4] = ti.Vector([1,5,7,3])
-        self.faces[5] = ti.Vector([5,4,6,7])
+        self.faces[0] = ti.Vector([0, 1, 3, 2])
+        self.faces[1] = ti.Vector([4, 5, 1, 0])
+        self.faces[2] = ti.Vector([2, 3, 7, 6])
+        self.faces[3] = ti.Vector([4, 0, 2, 6])
+        self.faces[4] = ti.Vector([1, 5, 7, 3])
+        self.faces[5] = ti.Vector([5, 4, 6, 7])
         for i in range(6):
             self.indices[i * 6 + 0] = self.faces[i][0]
             self.indices[i * 6 + 1] = self.faces[i][1]
@@ -395,9 +392,11 @@ class Cube:
             self.indices[i * 6 + 3] = self.faces[i][2]
             self.indices[i * 6 + 4] = self.faces[i][3]
             self.indices[i * 6 + 5] = self.faces[i][0]
-        
 
-arr = np.zeros(shape = (10, 8, 3))
+
+arr = np.zeros(shape=(10, 8, 3))
+
+
 def booknote(a):
     global arr
     arr[:-1] = arr[1:]
@@ -405,18 +404,20 @@ def booknote(a):
     trajectory.from_numpy(arr.reshape(-1, 3))
     # return arr.reshape(-1,3)
 
+
 def main():
-    window = ti.ui.Window("Articulated Multibody Simualtion", (800, 800), vsync = True)
+    window = ti.ui.Window(
+        "Articulated Multibody Simualtion", (800, 800), vsync=True)
     canvas = window.get_canvas()
     canvas.set_background_color((0, 0, 0))
     scene = ti.ui.Scene()
     camera = ti.ui.Camera()
-    camera_pos = np.array([0.0,0.0,3.0])
+    camera_pos = np.array([0.0, 0.0, 3.0])
     camera_dir = np.array([0.0, 0.0, -1.0])
-    
-    cube = Cube(omega = [10.0, 10.0, 10.0])
-    
-    mouse_staled = np.zeros(2, dtype = np.float32)
+
+    cube = Cube(omega=[10.0, 10.0, 10.0])
+
+    mouse_staled = np.zeros(2, dtype=np.float32)
     ts = 0
     while window.running:
         mouse = np.array([*window.get_cursor_pos(), 0.0])
@@ -438,24 +439,25 @@ def main():
         dmouse = mouse - mouse_staled
         camera_dir += dmouse * 1.0
         mouse_staled = mouse
-            
-        
-        
+
         camera.position(*camera_pos)
         camera.lookat(*(camera_pos + camera_dir))
         scene.set_camera(camera)
-        
-        scene.point_light(pos = (0,1,2), color = (1,1,1))
+
+        scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
         scene.ambient_light((0.5, 0.5, 0.5))
         for i in range(10):
             cube.substep()
 
-        scene.mesh(cube.v_transformed, cube.indices, two_sided = True, show_wireframe = False)            
+        scene.mesh(cube.v_transformed, cube.indices,
+                   two_sided=True, show_wireframe=False)
         if ts % per_trace == 0:
             t = booknote(cube.v_transformed.to_numpy())
-        scene.particles(trajectory, radius = 0.01, color = (1.0, 0.0, 0.0))
-        scene.particles(cube.v_transformed, radius = 0.05, color = (1.0, 0.0, 0.0))
+        scene.particles(trajectory, radius=0.01, color=(1.0, 0.0, 0.0))
+        scene.particles(cube.v_transformed, radius=0.05, color=(1.0, 0.0, 0.0))
         canvas.scene(scene)
         window.show()
         ts += 1
+
+
 main()
