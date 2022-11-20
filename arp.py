[fix] one cube stable rotation, need feedback

arp.py
@@ -119,50 +119,21 @@ def rotation_dot(a, b, c, d1, d2, d3):
     return R_dot
 
 @ti.kernel
-def Rq_r(q: ti.template(), 
+def skew_Rr(R0: ti.template(), 
     r0: float, 
     r1: float, 
     r2: float, 
     ret: ti.types.ndarray()):
-    '''
-    
-    '''
-    a = q[None][3]
-    b = q[None][4]
-    c = q[None][5]
-
-    # _r = ti.Vector([r[0], r[1], r[2]])
     _r = ti.Vector([r0, r1, r2])
-    s1 = sin(a)
-    s2 = sin(b)
-    s3 = sin(c)
 
-    c1 = cos(a)
-    c2 = cos(b)
-    c3 = cos(c)
-
-    pR_pa = ti.Matrix([
-        [-s1 * c2, -s1 * s2 * s3 - c3 * c1, c1 * s3 + -s1 * c3 * s2],
-        [c2 * c1, -s1 * c3 + c1 * s2 * s3, c3 * c1 * s2 - -s1 * s3],
-        [0, 0, 0]
-    ])
-    pR_pb = ti.Matrix([
-        [c1 * -s2, c1 * c2 * s3, c1 * c3 * c2],
-        [-s2 * s1, s1 * c2 * s3, c3 * s1 * c2],
-        [-c2, -s2 * s3, -s2 * c3]
-    ])
-    pR_pc = ti.Matrix([
-        [0, c1 * s2 * c3 - -s3 * s1, s1 * c3 + c1 * -s3 * s2],
-        [0, c1 * -s3 + s1 * s2 * c3, -s3 * s1 * s2 - c1 * c3],
-        [0, c2 * c3, c2 * -s3]
-    ])
-    t = ti.Matrix.cols([pR_pa @ _r, pR_pb @ _r, pR_pc @ _r])
+    R = R0[None]
+    M = skew(R @ _r)
 
     for i, j in ti.static(ti.ndrange(3, 3)):
-        ret[i, j] = t[i, j]
+        ret[i, j] = M[i, j]
 
 @ti.kernel
-def Rq_r_dot(q: ti.template(), q_dot: ti.template(), 
+def wR_dot_r(R0: ti.template(), q_dot: ti.template(), 
     r0: float, 
     r1: float, 
     r2: float, 
@@ -170,78 +141,10 @@ def Rq_r_dot(q: ti.template(), q_dot: ti.template(),
     '''
     
     '''
-    a, b, c = q[None][3], q[None][4], q[None][5]
-    d1, d2, d3 = q_dot[None][3], q_dot[None][4], q_dot[None][5]
-
-    s1 = sin(a)
-    s2 = sin(b)
-    s3 = sin(c)
-
-    c1 = cos(a)
-    c2 = cos(b)
-    c3 = cos(c)
-
+    omega = ti.Vector([q_dot[None][3], q_dot[None][4], q_dot[None][5]])
     r = ti.Vector([r0, r1, r2])
-    R = ti.Matrix([
-        [c1 * c2, c1 * s2 * s3 - c3 * s1, s1 * s3 + c1 * c3 * s2],
-        [c2 * s1, c1 * c3 + s1 * s2 * s3, c3 * s1 * s2 - c1 * s3],
-        [-s2, c2 * s3, c2 * c3]
-    ])
-    pR_pa = ti.Matrix([
-        [-s1 * c2, -s1 * s2 * s3 - c3 * c1, c1 * s3 + -s1 * c3 * s2],
-        [c2 * c1, -s1 * c3 + c1 * s2 * s3, c3 * c1 * s2 - -s1 * s3],
-        [0, 0, 0]
-    ])
-    pR_pb = ti.Matrix([
-        [c1 * -s2, c1 * c2 * s3, c1 * c3 * c2],
-        [-s2 * s1, s1 * c2 * s3, c3 * s1 * c2],
-        [-c2, -s2 * s3, -s2 * c3]
-    ])
-    pR_pc = ti.Matrix([
-        [0, c1 * s2 * c3 - -s3 * s1, s1 * c3 + c1 * -s3 * s2],
-        [0, c1 * -s3 + s1 * s2 * c3, -s3 * s1 * s2 - c1 * c3],
-        [0, c2 * c3, c2 * -s3]
-    ])
-    R_dot = pR_pa * d1 + pR_pb * d2 + pR_pc * d3
-
-    pR2_paa = ti.Matrix([
-        [-c1 * c2, -c1 * s2 * s3 - c3 * -s1, -s1 * s3 + -c1 * c3 * s2],
-        [c2 * -s1, -c1 * c3 + -s1 * s2 * s3, c3 * -s1 * s2 - -c1 * s3],
-        [0, 0, 0]
-    ])
-
-    pR2_pab = ti.Matrix([
-        [-s1 * -s2, -s1 * c2 * s3, -s1 * c3 * c2],
-        [-s2 * c1, c1 * c2 * s3, c3 * c1 * c2],
-        [0, 0, 0]
-    ])
-    pR2_pbb = ti.Matrix([
-        [c1 * -c2, c1 * -s2 * s3, c1 * c3 * -s2],
-        [-c2 * s1, s1 * -s2 * s3, c3 * s1 * -s2],
-        [s2, -c2 * s3, -c2 * c3]
-    ])
-
-    pR2_pac = ti.Matrix([
-        [0, -s1 * s2 * c3 - -s3 * c1, c1 * c3 + -s1 * -s3 * s2],
-        [0, -s1 * -s3 + c1 * s2 * c3, -s3 * c1 * s2 - -s1 * c3],
-        [0, 0, 0]
-    ])
-    pR2_pbc = ti.Matrix([
-        [0, c1 * c2 * c3, c1 * -s3 * c2],
-        [0, s1 * c2 * c3, -s3 * s1 * c2],
-        [0, -s2 * c3, -s2 * -s3]
-    ])
-    pR2_pcc = ti.Matrix([
-        [0, c1 * s2 * -s3 - -c3 * s1, s1 * -s3 + c1 * -c3 * s2],
-        [0, c1 * -c3 + s1 * s2 * -s3, -c3 * s1 * s2 - c1 * -s3],
-        [0, c2 * -s3, c2 * -c3]
-    ])
-
-    dpR_pa = pR2_paa * d1 + pR2_pab * d2 + pR2_pac * d3
-    dpR_pb = pR2_pab * d1 + pR2_pbb * d2 + pR2_pbc * d3
-    dpR_pc = pR2_pac * d1 + pR2_pbc * d2 + pR2_pcc * d3
-
-    M = ti.Matrix.cols([dpR_pa @ r, dpR_pb @ r, dpR_pc @ r])
+    R = R0[None]
+    M = skew(skew(omega) @ R @ r)
 
     for i, j in ti.static(ti.ndrange(3, 3)):
         ret[i, j] = M[i, j]
@@ -829,26 +732,6 @@ class Cube:
         for c in self.children:
             c.particles(scene)
 
-    # TODO: solve constrained rigid body system
-    # @ti.kernel
-    # def fill_R0q_k(self):
-    #     '''
-    #     side effect: update R0
-    #     '''
-    #     _Rq = Rq(q[3], q[4], q[5])
-    #     if ti.static(self.parent is None):
-    #         globals.R0q_pk[self.id] = _Rq
-    #     else:
-    #         q = self.q[None]
-    #         R_k = rotation(q[3], q[4], q[5])
-
-    #         R0_pk = self.parent.R0[None]
-    #         for i in globals.R0q_pk:
-    #             if ti.static(i == self.id):
-    #                 globals.R0q_k[i] = R0_pk @ _Rq
-    #             else :
-    #                 globals.R0q_k[i] = globals.R0q_pk[i] @ R_k
-
     def fill_Jc(self):
         global globals
         '''
@@ -857,50 +740,32 @@ class Cube:
         q layout:
         q_1[0:6], q_2[0:6],..., q_n[0:6], 
         '''
-        # Rq_rpkl = np.zeros((3, n_dofs), np.float32)
-        # Rq_rlk = np.zeros((3, n_dofs), np.float32)
-        
-        # self.fill_R0q_k()
-        # field_Mvp(globals.R0q_pk, ti.Vector(self.r_pkl_hat, float), Rq_rpkl)
-        # field_Mvp(globals.R0q_k, -ti.Vector(self.r_lk_hat, float), Rq_rlk)
-
-        # Jv_pk = np.zeros((3, n_dofs), np.float32)
-        # Jv_k = np.zeros((3, n_dofs), np.float32)
-
-        # pi0 = self.parent.id * 6
-        # i0 = self.id * 6
-        # Jv_pk[:, pi0 : pi0 + 3] += self.parent.m
-        # Jv_k[:, i0: i0 + 3] += self.m
-
-        # if self.parent is not None:
-        #     globals.Jc[(self.id - 1) * 3 : self.id * 3, :] = Jv_pk + Rq_rpkl 
-        #     # self.fill_Jvk()
-        #     globals.Jc[(self.id - 1) * 3 : self.id * 3, :] -= Jv_k + Rq_rlk
         
         pk = self.parent.id
         k = self.id
-        q_pk = self.parent.q
-        q_k = self.q
-        Rq_pk = np.zeros((3,3), np.float32)
-        Rq_k = np.zeros((3,3), np.float32)
+        # q_pk = self.parent.q
+        # q_k = self.q
+        Rr_pk = np.zeros((3,3), np.float32)
+        Rr_k = np.zeros((3,3), np.float32)
         lines = np.zeros((3, n_dofs), np.float32)
 
         # lines = globals.Jc[3 * (k -1) * 3 : 3 * k, :]
 
-        Rq_r(q_pk, 
+        skew_Rr(self.parent.R0, 
             self.r_pkl_hat[0], 
             self.r_pkl_hat[1], 
             self.r_pkl_hat[2], 
-            Rq_pk)
-        Rq_r(q_k, 
+            Rr_pk)
+
+        skew_Rr(self.R0, 
             -self.r_lk_hat[0],
             -self.r_lk_hat[1],
             -self.r_lk_hat[2],
-            Rq_k)
+            Rr_k)
         lines[:, 6 * pk: 6 * pk + 3] = np.identity(3, np.float32) 
         lines[:, 6 * k: 6 * k + 3] = -np.identity(3, np.float32) 
-        lines[:, 6 * pk + 3: 6 * pk + 6] = Rq_pk
-        lines[:, 6 * k + 3: 6 * k + 6] = -Rq_k
+        lines[:, 6 * pk + 3: 6 * pk + 6] = -Rr_pk
+        lines[:, 6 * k + 3: 6 * k + 6] = +Rr_k
 
         globals.Jc[3 * (k -1) * 3 : 3 * k, :] = lines
 
@@ -908,28 +773,28 @@ class Cube:
         global globals
         pk = self.parent.id
         k = self.id
-        q_pk = self.parent.q
-        q_k = self.q
+        # q_pk = self.parent.q
+        # q_k = self.q
         q_dot_pk = self.parent.q_dot
         q_dot_k = self.q_dot
-        Rq_dot_pk = np.zeros((3,3), np.float32)
-        Rq_dot_k = np.zeros((3,3), np.float32)
+        R_dot_r_pk = np.zeros((3,3), np.float32)
+        R_dot_r_k = np.zeros((3,3), np.float32)
         lines = np.zeros((3, n_dofs), np.float32)
 
-        Rq_r_dot(q_pk, q_dot_pk, 
+        wR_dot_r(self.parent.R0, q_dot_pk, 
             self.r_pkl_hat[0], 
             self.r_pkl_hat[1], 
             self.r_pkl_hat[2], 
-            Rq_dot_pk)
+            R_dot_r_pk)
 
-        Rq_r_dot(q_k, q_dot_k, 
+        wR_dot_r(self.R0, q_dot_k, 
             -self.r_lk_hat[0], 
             -self.r_lk_hat[1], 
             -self.r_lk_hat[2], 
-            Rq_dot_k)
+            R_dot_r_k)
 
-        lines[:, 6 * pk + 3: 6 * pk + 6] = Rq_dot_pk
-        lines[:, 6 * k + 3: 6 * k + 6] = -Rq_dot_k
+        lines[:, 6 * pk + 3: 6 * pk + 6] = -R_dot_r_pk
+        lines[:, 6 * k + 3: 6 * k + 6] = R_dot_r_k
 
         globals.Jc_dot[3 * (k -1) * 3 : 3 * k, :] = lines
 
@@ -941,6 +806,16 @@ class Cube:
             c.fill_W(W)
 
     def solve_sytem(self):
+        '''
+        C.. = J. q. + J W Q
+        q. = (v_c0, omega_0, ..., v_cn, omega_n)
+        W = diag(1/m0, 1/Ic0, ..., )
+        Q = (f0, tau0, ..., )
+
+              p(k)              k
+        J. = (0, -[[w]Rr], ..., 0 , -[[w]Rr])
+        J  = (I, -[Rr],    ..., -I, -[[w]Rr])
+        '''
         diag_W = np.zeros((n_dofs), np.float32)
         self.fill_W(diag_W)
         JcWJcT = globals.Jc @ np.diag(diag_W) @ globals.Jc.T
@@ -959,9 +834,12 @@ class Cube:
         return q__
     
     @ti.kernel
-    def fill_R0(self):
-        q = self.q[None]
-        self.R0[None] = rotation(q[3], q[4], q[5])
+    def fill_R0(self, dt: float):
+        # q = self.q[None]
+        # self.R0[None] = rotation(q[3], q[4], q[5])
+        q_ = self.q_dot[None]
+        omega = ti.Vector([q_[3], q_[4], q_[5]])
+        self.R0[None] += skew(omega) @ self.R0[None] * dt
 
     def top_down_constrained(self):
         global globals
@@ -976,7 +854,7 @@ class Cube:
             self.fill_Jc_dot()
 
         dxc = globals.q_dot[self.id * 6: self.id * 6 + 3].reshape((-1, 1)) * dt
-        self.fill_R0()
+        self.fill_R0(dt)
         self.project_vertices(dxc)
 
         for c in self.children: